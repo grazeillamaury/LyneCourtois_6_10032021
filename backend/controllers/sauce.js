1697bbb0f8665e870518574266a1582d65915b0c
graziellamaury <lynecourtois@free.fr>
2021-03-24 10:05:26 +0100

    route post pour les likes créé

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -68,3 +68,79 @@ exports.deleteSauce = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }));
 };
+
+exports.addLike = (req, res, next) => {
+
+  //On supprime L'utilisateur dans les tableaux usersLiked et usersDisliked
+  Sauce.updateOne({ _id: req.params.id },
+    { $pull: { usersLiked: req.body.userId, usersDisliked: req.body.userId }},
+    { multi: true })
+  .then(() => {
+
+    //Si 1
+    if (req.body.like > 0) {
+      console.log('Utilisateur aime')
+
+      //On ajoute l'utilisateur dans usersLiked
+      Sauce.updateOne({ _id: req.params.id },
+        { $push: { usersLiked: req.body.userId }})
+      .then(() => {
+
+        //Récupération des données
+        Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          console.log(sauce.usersLiked.length)
+          console.log(sauce.usersDisliked.length)
+
+          //Initialisation des Likes/dislikes
+          Sauce.updateOne({ _id: req.params.id },
+            { likes: sauce.usersLiked.length, dislikes : sauce.usersDisliked.length})
+          .then(() => res.status(200).json({ message: "L'utilisateur aime/aimes pas"}))
+          .catch(error => res.status(400).json({ error }));
+        })
+        .catch((error) => {res.status(404).json({error: error})});
+      })
+      .catch(error => res.status(400).json({ error }));
+
+    //Si -1
+    } else if (req.body.like < 0) {
+      console.log('Utilisateur n\'aime pas')
+
+      //On ajoute l'utilisateur dans usersLiked
+      Sauce.updateOne({ _id: req.params.id },
+        { $push: { usersDisliked: req.body.userId }})
+      .then(() => {
+
+        //Récupération des données
+        Sauce.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          console.log(sauce.usersLiked.length)
+          console.log(sauce.usersDisliked.length)
+
+          //Initialisation des Likes/dislikes
+          Sauce.updateOne({ _id: req.params.id },
+            { likes: sauce.usersLiked.length, dislikes : sauce.usersDisliked.length})
+          .then(() => res.status(200).json({ message: "L'utilisateur aime/aimes pas"}))
+          .catch(error => res.status(400).json({ error }));
+        })
+        .catch((error) => {res.status(404).json({error: error})});
+      })
+      .catch(error => res.status(400).json({ error }));
+    }else{
+      //Récupération des données
+      Sauce.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        console.log(sauce.usersLiked.length)
+        console.log(sauce.usersDisliked.length)
+
+        //Initialisation des Likes/dislikes
+        Sauce.updateOne({ _id: req.params.id },
+          { likes: sauce.usersLiked.length, dislikes : sauce.usersDisliked.length})
+        .then(() => res.status(200).json({ message: "L'utilisateur aime/aimes pas"}))
+        .catch(error => res.status(400).json({ error }));
+      })
+      .catch((error) => {res.status(404).json({error: error})});
+    };
+  })
+  .catch(error => res.status(400).json({ error }));
+};
