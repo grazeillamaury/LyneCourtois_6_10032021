8b764eca54ec9d226457721374c6129fa2aa7c0a
graziellamaury <lynecourtois@free.fr>
2021-03-30 10:25:58 +0200

    masquage de l'email avec maskdata

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,13 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const maskData = require('maskdata');
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
-        email: req.body.email,
+        email: maskData.maskEmail2(req.body.email),
         password: hash
       });
       user.save()
@@ -17,7 +18,7 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+  User.findOne({ email: maskData.maskEmail2(req.body.email)})
     .then(user => {
       if (!user) {
         return res.status(401).json({ error: 'Utilisateur non trouvé !' });
