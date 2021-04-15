e13facab290eb3913257ac951c32cffb677c0e11
graziellamaury <lynecourtois@free.fr>
2021-04-15 10:11:00 +0200

    ajout des modules express-rate-limit et password-validator

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,8 +2,23 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const maskData = require('maskdata');
+const passwordValidator = require('password-validator');
+
+const schemaPassValid = new passwordValidator();
+
+schemaPassValid
+.is().min(8)
+.is().max(50)
+.has().uppercase()
+.has().lowercase()
+.has().digits(2)
+.has().not().spaces()
+.is().not().oneOf(['Passw0rd', 'Password123']);
 
 exports.signup = (req, res, next) => {
+  if (!schemaPassValid.validate(req.body.password)) {
+    res.status(401).json({message:"Sécurité du mot de passe faible. Il doit contenir au moins 8 caractère, des majuscules et deux chiffres"})
+  }
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
